898dc91a36393242921785d7b04496fd255852e1
Srdjan Popic <srdan.popic@tttech.com>
2024-03-22 16:39:14 +0100

    [srdjan] initial integration tests action 11

--- test_run.sh
+++ test_run.sh
@@ -2,4 +2,4 @@ export DATABASE_URL="sqlite:///data.db"
 export JWT_KEY="15835791052248868013867479752485955126834265180411671240111600063995691199944"
 flask db upgrade
 flask run
-# rm ./instance/data.db
+rm -f ./instance/data.db
