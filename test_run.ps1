65941faa6884b05a447d1c4c8fafd76b8f463c30
Srdjan Popic <srdan.popic@tttech.com>
2024-03-28 08:47:42 +0100

    [srdjan] implementation of the waitress-server

--- test_run.ps1
+++ test_run.ps1
@@ -2,5 +2,5 @@ del .\instance\data.db
 $env:DATABASE_URL="sqlite:///data.db"
 $env:JWT_KEY="15835791052248868013867479752485955126834265180411671240111600063995691199944"
 flask db upgrade
-flask run
+waitress-serve --port=5000 wsgi:app
 del .\instance\data.db
--- test_run.sh
+++ test_run.sh
@@ -1,5 +1,5 @@
 export DATABASE_URL="sqlite:///data.db"
 export JWT_KEY="15835791052248868013867479752485955126834265180411671240111600063995691199944"
 flask db upgrade
-flask run
+waitress-serve --port=5000 wsgi:app
 rm -f ./instance/data.db
